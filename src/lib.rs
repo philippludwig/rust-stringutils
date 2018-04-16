3efcb9751177539d243076cabe4dfe136e4b7e98
Philipp Ludwig <git@philippludwig.net>
2018-04-16 14:33:12 +0200

    Added stringutils::byte_array_to_string

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,30 @@
+use std::fmt::Write;
+
+/// Convert a byte array to a ``String``.
+///
+/// # Examples
+///
+/// ```
+/// let byte_array: Vec<u8> = vec!(0x0F, 0x5F, 0xAA);
+/// let repr: String = stringutils::byte_array_to_string(&byte_array);
+///
+/// println!("Bytes: {}", repr);
+/// ```
+pub fn byte_array_to_string(bytes: &[u8]) -> String {
+	let mut repr = String::new();
+	for x in bytes {
+		write!(&mut repr, "{:02x}", x).unwrap();
+	}
+	repr
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+	#[test]
+	fn test_ba_to_string() {
+		let byte_array = vec!(0x0au8, 0x20u8, 0x4Fu8);
+		let repr = super::byte_array_to_string(&byte_array);
+		assert_eq!(repr, String::from("0a204f"));
+		println!("{}", repr);
+	}
 }
